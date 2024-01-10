6625006eccdc348d12a0231bacb7477738aa3c61
AmirSoftwareDeveloper <70962313+AmirSoftwareDeveloper@users.noreply.github.com>
2024-01-10 14:41:00 -0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,10 @@ import defaultTheme from "tailwindcss/defaultTheme";
 
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {
